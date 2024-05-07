7c9d0a74a13974af438de80ca69d350294772ec0
nguyen378 <82461544+nguyen378@users.noreply.github.com>
2024-05-07 22:10:06 +0700

    init

---
+++ Capture.py
@@ -0,0 +1,27 @@
+import time
+from PIL import ImageGrab
+import os
+
+def capture_screen(region):
+    return ImageGrab.grab(region)
+
+def save_screenshot(img, folder, filename):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    img.save(os.path.join(folder, filename))
+
+def main():
+    # Define the region to capture (left, top, right, bottom)
+    region = (350, 150, 1450, 900)
+
+    # Capture and save screenshots every 10 seconds
+    while True:
+        current_time = time.strftime("%Y%m%d_%H%M%S")
+        filename = f"{current_time}.png"
+        screenshot = capture_screen(region)
+        save_screenshot(screenshot,'folder', filename)
+        print(f"Screenshot captured: {filename}")
+        time.sleep(1)  # Capture every 10 seconds
+
+if __name__ == "__main__":
+    main()
