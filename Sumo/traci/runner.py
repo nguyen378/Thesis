ddb4e58baba4c66ce5846c9d8926388594ac7ae5
nguyen378 <82461544+nguyen378@users.noreply.github.com>
2024-05-13 13:40:23 +0700

    update

---
+++ Sumo/traci/runner.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import optparse
+import random
+
+# we need to import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
+def run():
+    
+    return 0
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+        
+    traci.start([sumoBinary, "-c", "F:\Project\Thesis\Thesis\Sumo/traci/2024-04-27-16-42-53\osm.sumocfg"])
+
