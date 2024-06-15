28a6143231d4a295c6e20a35b323c55c66736948
Nguyễn Ngọc Trí Vĩ <nntrivi2001@gmail.com>
2024-06-16 05:01:24 +0700

    Webster Cycle Length

--- Sumo/traci/runner.py
+++ Sumo/traci/runner.py
@@ -23,6 +23,27 @@ import time
 from PIL import ImageGrab
 
 
+def websters(y_crit, L, c_min, c_max, num_phases):
+    """Calculate the optimal cycle length using Webster's formula."""
+    Y = sum(y_crit)
+    if Y > 0.85:
+        Y = 0.85
+    elif Y == 0.0:
+        Y = 0.01
+
+    # Compute the cycle time
+    C = int(((1.5 * L*num_phases) + 5) / (1.0 - Y))
+    print("C = ", C)
+
+    # Constrain the cycle time within minimum and maximum bounds
+    if C > c_max:
+        C = c_max
+    elif C < c_min:
+        C = c_min
+
+    return C
+
+
 def run():
     """execute the TraCI control loop"""
     step = 0
@@ -32,7 +53,7 @@ def run():
     tlc = TrafficLightControl("J28")
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
-        total_time = 0
+        current_phase = traci.trafficlight.getPhase("J28")
         if red_light_time == 2:
             print("Chup hinh doi pha")
             # phân loại mật độ giao thông
@@ -41,29 +62,44 @@ def run():
             weight = dt.calculate_weight(result)
             if weight == 0:
                 weight = 1
+            
             if(weight < 12):
-                total_time = 32 *2
+                c_min = 20  # Minimum cycle length in seconds
+                c_max = 32*2  # Maximum cycle length in seconds
                 print('vang')
             elif (weight <30):
-                total_time = 45 *2
+                c_min = 20
+                c_max = 45*2 
                 print('it')
             else:
-                total_time = 60 *2
+                c_min = 20
+                c_max = 60*2 
                 print('dong')
 
-        horizontal_road_image, vertical_road_image = cp.capture_road3T(screen)
-        horizontal_result = dt.predict( horizontal_road_image)
-        vertical_result = dt.predict( vertical_road_image)
-        horizontal_weight = dt.calculate_weight(horizontal_result)
-        vertical_weight = dt.calculate_weight(vertical_result)
-        
-        time1 = round((horizontal_weight /weight) * total_time)
-        time2 = round((vertical_weight /weight) * total_time)
-        phases = tlc.create_phases_3(time1, time2)
-        tlc.set_traffic_light_cycle( phases)
-        print("ngang:", str(horizontal_weight) +' '+ str(time1), "doc", str(vertical_weight) +' '+ str(time2))
+            # Calculate y_crit using the updated vehicle counts
+            sat_flow = 1500  # Saturation flow rate in vehicles per hour | Default = 1800
+            y_crit_value = [tlc.calculate_y_crit(sat_flow)]  # Consider using dynamic saturation flow values
+            L = 5  # Lost time (red + yellow) in seconds, adjust as necessary
+            total_time = websters(y_crit_value, L, c_min, c_max, num_phases = 4)
+            print("Total time: ",total_time, "y_crit_value: ", y_crit_value)
+            
+
+            horizontal_road_image, vertical_road_image = cp.capture_road3T(screen)
+            horizontal_result = dt.predict( horizontal_road_image)
+            vertical_result = dt.predict( vertical_road_image)
+            horizontal_weight = dt.calculate_weight(horizontal_result)
+            vertical_weight = dt.calculate_weight(vertical_result)
+            
+            time1 = round((horizontal_weight /weight) * total_time)
+            time2 = round((vertical_weight /weight) * total_time)
+            phases = tlc.create_phases_3(time1, time2)
+            tlc.set_traffic_light_cycle( phases)
+
+            print("Ngang:", str(horizontal_weight) +' time ngang:'+ str(time1), "Doc", str(vertical_weight) +' time doc:'+ str(time2))
         
         if traci.trafficlight.getPhase("J28") == 3:
+            # Update vehicle counts for the current phase
+            tlc.update_vehicle_counts(current_phase)
             # Nếu đèn giao thông là đèn đỏ
             if red_light_time is None or red_light_time > 2:
                 print(traci.trafficlight.getNextSwitch("J28") - traci.simulation.getTime())
@@ -71,6 +107,8 @@ def run():
         else:
             # Nếu đèn giao thông không phải là đèn đỏ, đặt lại red_light_time
             red_light_time = None
+            # Update vehicle counts for the current phase
+            tlc.update_vehicle_counts(current_phase)
 
         step += 1
     traci.close()
