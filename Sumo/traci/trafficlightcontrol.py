eccd9afeafa6a5338e77d3514428cb9d73c97d1f
Nguyễn Ngọc Trí Vĩ <nntrivi2001@gmail.com>
2024-06-17 15:23:16 +0700

    Updated

--- Sumo/traci/trafficlightcontrol.py
+++ Sumo/traci/trafficlightcontrol.py
@@ -21,28 +21,6 @@ class TrafficLightControl:
         
         return total_waiting_time
 
-    def calculate_travel_time(self):
-        """Calculate the total travel time through the junction for vehicles."""
-        travel_times = []
-        controlled_lanes = traci.trafficlight.getControlledLanes(self.trafficlight_id)
-        
-        for lane in controlled_lanes:
-            vehicles = traci.lane.getLastStepVehicleIDs(lane)
-            for vehicle in vehicles:
-                if vehicle not in self.vehicle_entry_times:
-                    # Record the entry time of the vehicle into the junction area
-                    self.vehicle_entry_times[vehicle] = traci.simulation.getTime()
-                else:
-                    # Calculate travel time if the vehicle has exited the junction
-                    if traci.vehicle.getRoadID(vehicle) not in controlled_lanes:
-                        entry_time = self.vehicle_entry_times.pop(vehicle, None)
-                        if entry_time is not None:
-                            travel_time = traci.simulation.getTime() - entry_time
-                            travel_times.append(travel_time)
-        
-        total_travel_time = sum(travel_times)
-        return total_travel_time
-    
     
     def create_phases(self, num_intersections, time_1, time_2, time_3=None, time_4=None, time_5=None):
         if num_intersections == 3:
